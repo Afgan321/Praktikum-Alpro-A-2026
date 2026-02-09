ba4ed197906ef4a91e6a790c217d6176c043b0e1
Afgan321 <fahrulsiagian@gmail.com>
2026-02-09 21:52:55 +0700

    hebat

---
+++ LATIHAN 2/NOMOR 1.py
@@ -0,0 +1,14 @@
+#LATIHAN 1
+
+
+b  = 21
+if (b % 3 == 0 and b % 5 == 0 ):
+  print("FizzBuzz")
+if( b % 3 == 0):
+  print("fizz")
+if( b % 5 == 0):
+  print("buzz")
+if (b % 7 == 0):
+  print("seven")
+if (b % 3 != 0 and b % 5 != 0 and b % 7 != 0 ):
+  print(b)
